1f2974927b3a6be8e0a40e4c7ba6557ee3908a0c
Boggs <hello.boggs@gmail.com>
2015-12-07 16:35:03 +0800

    Try.

--- apply.sh
+++ apply.sh
@@ -2,7 +2,7 @@
 ########## Variables
 
 dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
+# olddir=~/dotfiles_old             # old dotfiles backup directory
 
 # list of files/folders to symlink in homedir
 files=".zshrc .vimrc .vim .tmuxinator .tmux.conf .fonts .oh-my-zsh .i3 .i3blocks.conf i3blocks .xcolors .dircolors assets .ideavimrc links"
@@ -10,9 +10,9 @@ files=".zshrc .vimrc .vim .tmuxinator .tmux.conf .fonts .oh-my-zsh .i3 .i3blocks
 ##########
 
 # create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
+# echo "Creating $olddir for backup of any existing dotfiles in ~"
+# mkdir -p $olddir
+# echo "...done"
 
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
@@ -21,8 +21,8 @@ echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/$file ~/dotfiles_old/
+    # echo "Moving any existing dotfiles from ~ to $olddir"
+    # mv ~/$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/$file
 done
